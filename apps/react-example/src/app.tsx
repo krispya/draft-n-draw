fa3b44e4eda5c3324db4ad701d7d39d92b2a5c02
Kris Baumgartner <kjbaumgartner@gmail.com>
2022-12-13 20:37:05 -0800

    Changing case pt. 2

---
+++ apps/react-example/src/app.tsx
@@ -0,0 +1,42 @@
+import { Drafter, useDrafter } from '@draft-n-draw/react';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Suspense, useEffect, useRef } from 'react';
+import './app.css';
+import * as THREE from 'three';
+
+function InnerApp() {
+  const spotLightRef = useRef<THREE.SpotLight>(null!);
+  const drafter = useDrafter();
+
+  // useFrame(() => {
+  //   if (spotLightRef.current) drafter.drawSpotlight(spotLightRef.current);
+  // });
+
+  useEffect(() => {
+    drafter.drawSpotlight(spotLightRef.current, { persist: true });
+    setTimeout(() => drafter.dispose(spotLightRef.current), 2000);
+    return () => drafter.dispose(spotLightRef.current);
+  });
+
+  return (
+    <>
+      <OrbitControls />
+      <spotLight ref={spotLightRef} />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Canvas>
+      <Suspense>
+        <Drafter>
+          <InnerApp />
+        </Drafter>
+      </Suspense>
+    </Canvas>
+  );
+}
+
+export default App;
